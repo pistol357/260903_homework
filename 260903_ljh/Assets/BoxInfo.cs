21d43e79f026400221e78748ce9560f16a8f6b3f
pistol357 <jeonghan615@gmail.com>
2026-09-03 17:41:53 +0900

    [Feat] 과제 중간 저장

---
+++ 260903_ljh/Assets/BoxInfo.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BoxInfo : MonoBehaviour
+{
+    [SerializeField] private string _boxName = "기본 상자";
+
+    private void Start()
+    {
+
+    }
+
+    private void OnDestroy()
+    {
+
+    }
+}
